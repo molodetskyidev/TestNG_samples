e17536030d25a1a0071bbca035487d07b458dbd1
Oleksandr Molodetskyi <molodetskyidev@gmail.com>
2017-12-23 14:48:49 +0200

    added explanation for ParametersTest and ExcelDataProvider

--- src/main/java/testng_samples/Explanation_ParametersTest.java
+++ src/main/java/testng_samples/Explanation_ParametersTest.java
@@ -1,7 +1,20 @@
 package testng_samples;
 
-/*
- * 
+/* Parameters defined by DataProvider are covered by Explanation_Annotations.java
+ * Here it is covered parameters defined by testng.xml
+ * <suite name="Suite"> <!-- Suite -->
+ * <parameter name="first-name"  value="Cedric"/>
+ *	<test name="Suite Test1"> <!-- Test -->
+ * in test should be @Parameter annotation:
+ * @Parameters({ "first-name" })
+ * and test method should have parameter
+ * 	public void TestWithParameter(String firstName) {
+ * ...
+ * There could be also optional parameters
+ * @Parameters({ "first-name", "last-name" })
+ * optional parameter should have default value which is used while parameter is not set in testng.xml
+ * public void TestWithOptionalParameter(String firstName, @Optional("Nickolson") String lastName) {
+ * ...
  */
 public class Explanation_ParametersTest {
 
